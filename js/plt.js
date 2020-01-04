b6fe781b4b4c8de7aa852cf7af6ffc82e4aa9b1a
kb- <fox_mhhyhq@Rcpt.at>
2020-01-05 00:16:55 +0100

    added titles on spectrograms
    added titles on spectrograms
    fixed spectrograms update

--- js/plt.js
+++ js/plt.js
@@ -40,13 +40,29 @@ export function surf(divId,x,y,z,type,zopt){//"id",1d[],1d[],2d[]
         },
         ortho: true
       },
-      xaxis: {autorange: "reversed"},
-      yaxis: {autorange: "reversed"},
-      zaxis: {autorange: true,
-        // tickvals:arraytools.linspace(-10,10,21),
-        // ticktext:arraytools.linspace(-10,10,21).map((e)=>Math.pow(10,e).toExponential())//exponential notation 1e-10:->1e10
-      }//zopt
-    }
+      //for 3D
+      xaxis: {
+        // autorange: "reversed",
+        title:     {text:"(s)"}
+      },
+      yaxis: {
+        // autorange: "reversed",
+        title:     {text:"(Hz)"}
+      },
+    },
+    //for 2D...
+    xaxis: {
+      // autorange: "reversed",
+      title:     {text:"(s)"}
+    },
+    yaxis: {
+      // autorange: "reversed",
+      title:     {text:"(Hz)"}
+    },
+    zaxis: {autorange: true,
+      // tickvals:arraytools.linspace(-10,10,21),
+      // ticktext:arraytools.linspace(-10,10,21).map((e)=>Math.pow(10,e).toExponential())//exponential notation 1e-10:->1e10
+    }//zopt
   };
   //var data = [{x:x, y:y, z: z, type: 'surface'}];
   var data = {x:x, y:y, z: z, colorscale:'Jet', type: type, hoverinfo: 'none'};
@@ -54,8 +70,8 @@ export function surf(divId,x,y,z,type,zopt){//"id",1d[],1d[],2d[]
   var minval = nj.min(spz);
   if(zopt.type=="log"){
     data.z = math.matrix(z).map((e,i,m)=>e==0?Math.log10(maxval)-5:math.log10(e)).toArray();//log10 z data
-    layout.scene.zaxis.tickvals = logbinslog;
-    layout.scene.zaxis.ticktext = logbins;
+    layout.zaxis.tickvals = logbinslog;
+    layout.zaxis.ticktext = logbins;
     data.colorbar = colorbar(minval, maxval);
     // data.colorbar = {
       // tickvals: logbinslog.concat([(Math.log10(minval)), (Math.log10(maxval))]),
@@ -63,7 +79,7 @@ export function surf(divId,x,y,z,type,zopt){//"id",1d[],1d[],2d[]
     // };
   }
   
-  
+  data.colorbar.title = {text:unit_conversion[displayed_spectrogram_idx].u};
   return Plotly.newPlot(divId, [data], layout);//returns promise
 }
 
@@ -87,6 +103,7 @@ export function updateSurf(divId,x,y,z,zopt){//"id",1d[],1d[],2d[]
       // ticktext: logbins.concat([minval.toExponential(3), maxval.toExponential(3)]),
     // }
   }
+  update.colorbar.title = {text:unit_conversion[displayed_spectrogram_idx].u};
   return Plotly.update(divId, update);
 }
 
@@ -95,7 +112,7 @@ function colorbar(minval, maxval){
   var maxbin = logbinslog.findIndex((e)=>e==Math.round(Math.log10(maxval)));
   var colorbar = {
     tickvals: logbinslog.concat([(Math.log10(minval)), (Math.log10(maxval))]).filter((e,i)=>(i != minbin && i != maxbin)),
-    ticktext: logbins.concat([minval.toExponential(3), maxval.toExponential(3)]).filter((e,i)=>(i != minbin && i != maxbin)),
+    ticktext: logbins.concat([minval.toExponential(2), maxval.toExponential(2)]).filter((e,i)=>(i != minbin && i != maxbin)),
   }
   
   return colorbar;
